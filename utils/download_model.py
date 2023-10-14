914c723c32e20f855b31d1798d206d9fab0858c8
Mwanja Joel <joelsilverworks@gmail.com>
2023-10-14 10:31:58 +0300

    Fixed env variables not loading

--- utils/download_model.py
+++ utils/download_model.py
@@ -1,6 +1,11 @@
 import os
 from huggingface_hub import hf_hub_download
 
+# load environment variables
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def get_model():
     # Download the model from the Hub
     llm_repo = os.getenv("HF_REPO")
